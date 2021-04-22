76bc931b008faeeab4d64b8ed33e3115702e72c6
Alan <alan.vitor@corp.globo.com>
2021-04-22 20:41:37 -0300

    Some driver object tests

--- tests/test_driver_gcp.py
+++ tests/test_driver_gcp.py
@@ -9,6 +9,36 @@ class FakeApp:
         return Response(body="Fake App")(environ, start_response)
 
 
+class FakeBlob:
+    def __init__(self, exists=True):
+        self._exists = exists
+        self.content_type = 'text/html'
+        self.etag = 'etag'
+
+    def exists(self):
+        return self._exists
+
+
+class FakeBucket:
+    def __init__(self, blob=FakeBlob(), blobs=[]):
+        self.blob = blob
+        self.blobs = blobs
+
+    def get_blob(self, *args, **kwargs):
+        return self.blob
+
+    def list_blobs(self, *args, **kwargs):
+        return self.blobs
+
+
+class FakeClient:
+    def __init__(self, bucket=FakeBucket()):
+        self.bucket = bucket
+
+    def get_bucket(self, *args, **kwargs):
+        return self.bucket
+
+
 class SwiftGCPDriverTestCase(TestCase):
 
     @classmethod
@@ -21,8 +51,17 @@ class SwiftGCPDriverTestCase(TestCase):
             'meta': {'cloud': 'gcp'}
         }
         cls.app = FakeApp()
+
+    def setUp(self):
         patch('swift_cloud.drivers.gcp.storage', Mock()).start()
         patch('swift_cloud.drivers.gcp.Credentials', Mock()).start()
+        self.mock_client = patch(
+            'swift_cloud.drivers.gcp.SwiftGCPDriver._get_client',
+            Mock()).start()
+        self.mock_client.return_value = FakeClient()
+
+    def tearDown(self):
+        patch.stopall()
 
     def _driver(self, path, method='GET'):
         environ = {
@@ -101,3 +140,39 @@ class SwiftGCPDriverTestCase(TestCase):
     def test_call_head_object(self, mock_head_object):
         res = self._driver('/v1/account/container/object', 'HEAD').response()
         mock_head_object.assert_called_once()
+
+    @patch('swift_cloud.drivers.gcp.SwiftGCPDriver.get_object')
+    def test_call_get_object(self, mock_get_object):
+        res = self._driver('/v1/account/container/object', 'GET').response()
+        mock_get_object.assert_called_once()
+
+    @patch('swift_cloud.drivers.gcp.SwiftGCPDriver.put_object')
+    def test_call_put_object(self, mock_put_object):
+        res = self._driver('/v1/account/container/object', 'PUT').response()
+        mock_put_object.assert_called_once()
+
+    @patch('swift_cloud.drivers.gcp.SwiftGCPDriver.post_object')
+    def test_call_post_object(self, mock_post_object):
+        res = self._driver('/v1/account/container/object', 'POST').response()
+        mock_post_object.assert_called_once()
+
+    @patch('swift_cloud.drivers.gcp.SwiftGCPDriver.delete_object')
+    def test_call_delete_object(self, mock_delete_object):
+        res = self._driver('/v1/account/container/object', 'DELETE').response()
+        mock_delete_object.assert_called_once()
+
+    def test_head_object_returns_204_status_code(self):
+        res = self._driver('/v1/account/container/object', 'HEAD').response()
+        self.assertEquals(res.status_int, 204)
+
+    def test_head_object_returns_404_if_blob_does_not_exists(self):
+        fake_blob = FakeBlob(exists=False)
+        fake_bucket = FakeBucket(blob=fake_blob)
+        self.mock_client.return_value = FakeClient(bucket=fake_bucket)
+        res = self._driver('/v1/account/container/object', 'HEAD').response()
+        self.assertEquals(res.status_int, 404)
+
+    def test_head_object_headers(self):
+        res = self._driver('/v1/account/container/object', 'HEAD').response()
+        self.assertIn('Content-Type', res.headers)
+        self.assertIn('Etag', res.headers)
