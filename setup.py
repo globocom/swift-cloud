24c9e14e350d00768cf6d4f854e3579606e0690e
Sergio Saules <sergios@corp.globo.com>
2023-02-15 09:37:09 -0300

    New version 0.2.33p

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='swift_cloud',
-    version='0.2.31',
+    version='0.2.33p',
     description='Middleware for Openstack Swift to store objecs on multiple cloud providers',
     author='Storm',
     author_email='storm@g.globo',
